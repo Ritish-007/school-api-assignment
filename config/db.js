364b5ebf1a5af74c02c0835435e4bb8ef53306fa
Ritish <british.1417@gmail.com>
2026-03-28 19:26:10 +0530

    final railway ready

--- config/db.js
+++ config/db.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql2')
-require ('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 connection.connect((err) => {
-    if(err){
-        console.error('Error connecting to the database:', err)
-    }
-    else{
-        console.log('Connected to the database successfully!')
-    }
-})
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Connected to Railway MySQL');
+  }
+});
 
-module.exports = connection
-
+module.exports = connection;
